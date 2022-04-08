074f1caf987bcf9f2a7186f15ea69ef9da615364
budihan <b.s.han@student.tudelft.nl>
2022-04-08 16:34:49 +0200

    test

--- bert_classification.py
+++ bert_classification.py
@@ -17,3 +17,22 @@ def read_webisData(test_size=0.2):
     Y = 'Some labels'
 
     return train_test_split(X, Y, test_size, shuffle=True)
+
+
+# # https://towardsdatascience.com/text-classification-with-bert-in-pytorch-887965e5820f
+
+# from transformers import BertModel, BertConfig, BertTokenizer
+
+# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# # Initializing a BERT bert-base-uncased style configuration
+# configuration = BertConfig()
+
+# # Initializing a model from the bert-base-uncased style configuration
+# model = BertModel(configuration)
+
+# # Accessing the model configuration
+# configuration = model.config
+
+# def train(model, train_data, val_data, learning_rate, epochs):
+#     train, val = 
